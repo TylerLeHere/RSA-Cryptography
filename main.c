346b96cd1e5339e5013c5afed172460c0930adbb
lucabolzonello <labolzonello@gmail.com>
2025-06-29 15:14:31 -0700

    Basic outline for what we may want to be doing.

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#define IntType u_int32_t
+
+int rsa_encrypt(IntType data, IntType P, IntType Q, IntType E) { return 0; }
+int rsa_decrypt(IntType data, IntType P, IntType Q, IntType D) { return 0; }
+
+IntType P = 0;
+IntType Q = 0;
+IntType E = 0;
+IntType D = 0;
+IntType text = 0;
